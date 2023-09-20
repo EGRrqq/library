b35e893307c247f10d9dfbd21743ba630141275a
EGRrqq <86194387+EGRrqq@users.noreply.github.com>
2023-09-19 23:04:27 -0500

    feat(js): add library function
    - library function contains:
    -- displayButton function
    -- addBookToLibrary function
    
    feat(css): center all body items
    feat(html): change page title

--- script.js
+++ script.js
@@ -1,1 +1,34 @@
-console.log('buh');
+const myLibrary = [];
+console.log('library:', myLibrary);
+
+(function library() {
+    displayButton();
+    addBookToLibrary();
+})()
+
+function displayButton() {
+    const body = document.querySelector('body');
+    const button = document.createElement('button');
+
+    button.textContent = 'add book';
+    body.appendChild(button);
+}
+
+function Book(title) {
+    this.title = title;
+}
+
+function addBookToLibrary() {
+    const button = document.querySelector('button');
+
+    button.addEventListener('click', function() {
+        const userInput = prompt('book title');
+
+        if (userInput) {
+            const newBook = new Book(userInput);
+            myLibrary.push(newBook);
+        }
+        console.log('library:', myLibrary);
+    })
+}
+
