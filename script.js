1362e3ecaddb9c4174b0f28a9d2ecb4101ed4a36
EGRrqq <86194387+EGRrqq@users.noreply.github.com>
2023-09-21 02:00:13 -0500

    feat(html): wrap form with dialog element
    feat(js): remove displayButton function
    feat(html): add button that displays a modal
    
    feat(js): add functions to open / close modal
    feat(js): modify addBookToLibrary function
    - add multiple input support
    
    feat(js): add Book.info() function to prototype
    feat(js): modify displayBook function
    - display Book.info() instead of Book.title

--- script.js
+++ script.js
@@ -2,10 +2,27 @@ const myLibrary = [];
 console.log('library:', myLibrary);
 
 (function library() {
-    displayButton();
+    openModal();
+    closeModal();
     addBookToLibrary();
     displayBooks();
 })()
+
+function openModal() {
+    const modal = document.getElementById('book-modal');
+    const showButton = document.getElementById('show-modal');
+
+    showButton.addEventListener('click', () => modal.showModal());
+}
+
+function closeModal() {
+    const modal = document.getElementById('book-modal');
+    const closeBtn = document.querySelector('.close-btn');
+
+    closeBtn.addEventListener('click', function() {
+        modal.close();
+    })
+}
 
 function displayBooks() {
     const body = document.querySelector('body');
@@ -13,43 +30,47 @@ function displayBooks() {
     body.appendChild(ul);
 
     Book.prototype.display = false;
-    const button = document.querySelector('button');
+    const form = document.querySelector('form');
 
-    button.addEventListener('click', function() {
+    form.addEventListener('submit', function() {
         const book = myLibrary[myLibrary.length - 1];
 
         if (!book.display) {
             book.display = true;
             const li = document.createElement('li');
-            li.textContent = book.title;
+            li.textContent = book.info();
             ul.appendChild(li);
         }
     })
 }
 
-function displayButton() {
-    const body = document.querySelector('body');
-    const button = document.createElement('button');
-
-    button.textContent = 'add book';
-    body.appendChild(button);
-}
-
-function Book(title) {
-    this.title = title;
-}
-
 function addBookToLibrary() {
-    const button = document.querySelector('button');
+    const form = document.querySelector('form');
 
-    button.addEventListener('click', function() {
-        const userInput = prompt('book title');
+    form.addEventListener('submit', function() {
+        let tempBook = new Book()
 
-        if (userInput) {
-            const newBook = new Book(userInput);
-            myLibrary.push(newBook);
-        }
-        console.log('library:', myLibrary);
+        const args = Object.keys(tempBook).map(key => {
+            if (key === 'read') {
+                return document.getElementById(key).checked;
+            }
+
+            return document.getElementById(key).value;
+        })
+        
+        myLibrary.push(new Book(...args));
+        tempBook = null;
+        console.log(myLibrary);
     })
 }
 
+function Book(title, author, pages, read) {
+    this.title = title;
+    this.author = author;
+    this.pages = pages;
+    this.read = read || false;
+}
+
+Book.prototype.info = function() {
+    return `${this.title} by ${this.author}, ${this.pages} pages, ${this.read}`
+}
