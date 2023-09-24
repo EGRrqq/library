09060b32370435158ebff3a91921a2a8561975b5
EGRrqq <86194387+EGRrqq@users.noreply.github.com>
2023-09-24 09:26:28 -0500

    feat: add prettier
    Until I set up prettier in my nvim, I need to install it locally

--- script.js
+++ script.js
@@ -1,197 +1,199 @@
-Book.prototype.info = function() {
-    return `${this.title} by ${this.author}, ${this.pages} pages, ${this.read}`
-}
+Book.prototype.info = function () {
+  return `${this.title} by ${this.author}, ${this.pages} pages, ${this.read}`;
+};
 
-const Harry = new Book('Signet 1984', 'George Orwell', 272, false);
-const Barry = new Book('Anchor The Stand', 'Stephen King', 1200, false);
-const Jarry = new Book('Clockwork Orange', 'Anthony Burgess', 240, true);
+const Harry = new Book("Signet 1984", "George Orwell", 272, false);
+const Barry = new Book("Anchor The Stand", "Stephen King", 1200, false);
+const Jarry = new Book("Clockwork Orange", "Anthony Burgess", 240, true);
 
 let myLibrary = [Harry, Barry, Jarry];
-console.log('library:', myLibrary);
+console.log("library:", myLibrary);
 
 (function library() {
-    openModal();
-    closeModal();
-    addBookToLibrary();
-    displayExistBook();
-    displayNewBook();
-})()
+  openModal();
+  closeModal();
+  addBookToLibrary();
+  displayExistBook();
+  displayNewBook();
+})();
 
 function openModal() {
-    const modal = document.getElementById('book-modal');
-    const showButton = document.getElementById('show-modal');
+  const modal = document.getElementById("book-modal");
+  const showButton = document.getElementById("show-modal");
 
-    showButton.addEventListener('click', () => modal.showModal());
+  showButton.addEventListener("click", () => modal.showModal());
 }
 
 function closeModal() {
-    const modal = document.getElementById('book-modal');
-    const closeBtn = document.getElementById('close-btn');
+  const modal = document.getElementById("book-modal");
+  const closeBtn = document.getElementById("close-btn");
 
-    closeBtn.addEventListener('click', () => modal.close());
-    window.addEventListener("click", (event) => {
-        if (event.target !== modal) {
-            return;
-        }
+  closeBtn.addEventListener("click", () => modal.close());
+  window.addEventListener("click", (event) => {
+    if (event.target !== modal) {
+      return;
+    }
 
-        modal.close();
-    });
+    modal.close();
+  });
 }
 
 function displayExistBook() {
-    const bookCollection = document.querySelector('.book-collection');
+  const bookCollection = document.querySelector(".book-collection");
 
-    for (let book of myLibrary) {
-        bookCard(book, bookCollection);
+  for (let book of myLibrary) {
+    bookCard(book, bookCollection);
 
-        const hr = document.createElement('hr');
-        bookCollection.appendChild(hr);
-    }
+    const hr = document.createElement("hr");
+    bookCollection.appendChild(hr);
+  }
 }
 
 function displayNewBook() {
-    const form = document.querySelector('form');
-    const bookCollection = document.querySelector('.book-collection');
+  const form = document.querySelector("form");
+  const bookCollection = document.querySelector(".book-collection");
 
-    form.addEventListener('submit', function() {
-        const book = myLibrary[myLibrary.length - 1];
-        bookCard(book, bookCollection);
+  form.addEventListener("submit", function () {
+    const book = myLibrary[myLibrary.length - 1];
+    bookCard(book, bookCollection);
 
-        const hr = document.createElement('hr');
-        bookCollection.appendChild(hr);
-    })
+    const hr = document.createElement("hr");
+    bookCollection.appendChild(hr);
+  });
 }
 
 function removeCardBtn(container) {
-    const removeBtn = document.createElement('button');
+  const removeBtn = document.createElement("button");
 
-    removeBtn.classList.add('btn', 'rounded', 'card-btn');
-    removeBtn.textContent = '-';
-    removeBtn.setAttribute('aria-label', 'remove book');
+  removeBtn.classList.add("btn", "rounded", "card-btn");
+  removeBtn.textContent = "-";
+  removeBtn.setAttribute("aria-label", "remove book");
 
-    removeBtn.addEventListener('click', function(event) {
-        const section = event.target.closest('section');
-        const bookId = section.dataset.id;
+  removeBtn.addEventListener("click", function (event) {
+    const section = event.target.closest("section");
+    const bookId = section.dataset.id;
 
-        myLibrary = myLibrary.filter(book => book.id !== bookId);
-        section.remove();
-        console.log(myLibrary);
-    })
+    myLibrary = myLibrary.filter((book) => book.id !== bookId);
+    section.remove();
+    console.log(myLibrary);
+  });
 
-    container.appendChild(removeBtn);
+  container.appendChild(removeBtn);
 }
 
 function toggleReadState(book, container) {
-    const toggleRead = document.createElement('input');
-    toggleRead.ariaLabel = 'book was read?';
-    toggleRead.name = "toggle read";
-    toggleRead.type = 'checkbox';
-    toggleRead.checked = book.read;
+  const toggleRead = document.createElement("input");
+  toggleRead.ariaLabel = "book was read?";
+  toggleRead.name = "toggle read";
+  toggleRead.type = "checkbox";
+  toggleRead.checked = book.read;
 
-    toggleRead.addEventListener('click', function(event) {
-        const section = event.target.closest('section');
-        const bookId = section.dataset.id;
+  toggleRead.addEventListener("click", function (event) {
+    const section = event.target.closest("section");
+    const bookId = section.dataset.id;
 
-        const li = section.querySelector(`li[data-content="read"]`);
-        myLibrary.find(book => book.id === bookId).read = toggleRead.checked
-        li.textContent = 'read: ' + toggleRead.checked;
-        console.log(myLibrary);
-    })
+    const li = section.querySelector(`li[data-content="read"]`);
+    myLibrary.find((book) => book.id === bookId).read = toggleRead.checked;
+    li.textContent = "read: " + toggleRead.checked;
+    console.log(myLibrary);
+  });
 
-    container.appendChild(toggleRead);
+  container.appendChild(toggleRead);
 }
 
 function bookCard(book, container) {
-    const section = document.createElement('section');
-    section.classList.add('book-card');
-    section.dataset.id = book.id;
+  const section = document.createElement("section");
+  section.classList.add("book-card");
+  section.dataset.id = book.id;
 
-    cardHeader(book, section);
-    cardBody(book, section);
-    cardFooter(book, section);
+  cardHeader(book, section);
+  cardBody(book, section);
+  cardFooter(book, section);
 
-    container.appendChild(section);
+  container.appendChild(section);
 }
 
 function cardFooter(book, container) {
-    const div = document.createElement('div');
-    div.classList.add('card-footer');
+  const div = document.createElement("div");
+  div.classList.add("card-footer");
 
-    removeCardBtn(div);
-    toggleReadState(book, div);
+  removeCardBtn(div);
+  toggleReadState(book, div);
 
-    container.appendChild(div);
+  container.appendChild(div);
 }
 
 function cardHeader(book, container) {
-    const div = document.createElement('div');
-    div.classList.add('card-header');
+  const div = document.createElement("div");
+  div.classList.add("card-header");
 
-    const span = document.createElement('span');
-    span.textContent = '###'
-    span.classList.add('bold');
+  const span = document.createElement("span");
+  span.textContent = "###";
+  span.classList.add("bold");
 
-    const h2 = document.createElement('h2');
-    h2.textContent = book.title;
+  const h2 = document.createElement("h2");
+  h2.textContent = book.title;
 
-    div.appendChild(span);
-    div.appendChild(h2);
+  div.appendChild(span);
+  div.appendChild(h2);
 
-    container.appendChild(div);
+  container.appendChild(div);
 }
 
 function cardBody(book, container) {
-    const div = document.createElement('div');
-    div.classList.add('card-body');
+  const div = document.createElement("div");
+  div.classList.add("card-body");
 
-    const hrFirst = document.createElement('hr');
-    const hrSecond = document.createElement('hr');
+  const hrFirst = document.createElement("hr");
+  const hrSecond = document.createElement("hr");
 
-    const ul = document.createElement('ul');
+  const ul = document.createElement("ul");
 
-    let tempBook = new Book();
+  let tempBook = new Book();
 
-    for (const key in tempBook) {
-        if (key === 'info' || key === 'id' || key === 'title') continue;
+  for (const key in tempBook) {
+    if (key === "info" || key === "id" || key === "title") continue;
 
-        const li = document.createElement('li');
-        li.textContent = `${key}: ${book[key]}`;
-        li.dataset.content = key;
-        ul.appendChild(li);
-    }
+    const li = document.createElement("li");
+    li.textContent = `${key}: ${book[key]}`;
+    li.dataset.content = key;
+    ul.appendChild(li);
+  }
 
-    tempBook = null;
+  tempBook = null;
 
-    div.appendChild(hrFirst);
-    div.appendChild(ul);
-    div.appendChild(hrSecond);
-    container.appendChild(div);
+  div.appendChild(hrFirst);
+  div.appendChild(ul);
+  div.appendChild(hrSecond);
+  container.appendChild(div);
 }
 
 function addBookToLibrary() {
-    const form = document.querySelector('form');
+  const form = document.querySelector("form");
 
-    form.addEventListener('submit', function() {
-        let tempBook = new Book();
+  form.addEventListener("submit", function () {
+    let tempBook = new Book();
 
-        const args = Object.keys(tempBook).filter(key => key !== 'id').map(key => {
-            if (key === 'read') {
-                return document.getElementById(key).checked;
-            }
+    const args = Object.keys(tempBook)
+      .filter((key) => key !== "id")
+      .map((key) => {
+        if (key === "read") {
+          return document.getElementById(key).checked;
+        }
 
-            return document.getElementById(key).value;
-        })
+        return document.getElementById(key).value;
+      });
 
-        myLibrary.push(new Book(...args));
-        tempBook = null;
-        console.log(myLibrary);
-    })
+    myLibrary.push(new Book(...args));
+    tempBook = null;
+    console.log(myLibrary);
+  });
 }
 
 function Book(title, author, pages, read) {
-    this.title = title;
-    this.author = author;
-    this.pages = pages;
-    this.read = read || false;
-    this.id = Math.random().toString(36).substring(2);
+  this.title = title;
+  this.author = author;
+  this.pages = pages;
+  this.read = read || false;
+  this.id = Math.random().toString(36).substring(2);
 }
--- visual.js
+++ visual.js
@@ -1,54 +1,58 @@
-let rupertWidth = parseInt(getComputedStyle(document.documentElement).getPropertyValue('--rupert-width'));
+let rupertWidth = parseInt(
+  getComputedStyle(document.documentElement).getPropertyValue("--rupert-width"),
+);
 let catsOnScreen = Math.ceil(window.innerWidth / rupertWidth);
 
 (function uuh() {
-    appendSpinningCat();
-})()
+  appendSpinningCat();
+})();
 
 function appendSpinningCat() {
-    const header = document.querySelector('header');
-    const catsContainer = document.createElement('section');
-    catsContainer.classList.add('track');
+  const header = document.querySelector("header");
+  const catsContainer = document.createElement("section");
+  catsContainer.classList.add("track");
 
-    for (let i = 0; i < catsOnScreen; i++) {
-        createSpiningCat(catsContainer);
-    }
+  for (let i = 0; i < catsOnScreen; i++) {
+    createSpiningCat(catsContainer);
+  }
 
-    header.appendChild(catsContainer);
-    window.addEventListener('resize', handleResize);
+  header.appendChild(catsContainer);
+  window.addEventListener("resize", handleResize);
 }
 
 function handleResize() {
-    const newCatsOnScreen = Math.ceil(window.innerWidth / rupertWidth);
+  const newCatsOnScreen = Math.ceil(window.innerWidth / rupertWidth);
 
-    if (newCatsOnScreen > catsOnScreen) {
-        const diff = newCatsOnScreen - catsOnScreen;
-        const catsContainer = document.querySelector('.track');
+  if (newCatsOnScreen > catsOnScreen) {
+    const diff = newCatsOnScreen - catsOnScreen;
+    const catsContainer = document.querySelector(".track");
 
-        for (let i = 0; i < diff; i++) {
-            createSpiningCat(catsContainer);
-        }
+    for (let i = 0; i < diff; i++) {
+      createSpiningCat(catsContainer);
     }
+  }
 
-    if (newCatsOnScreen < catsOnScreen) {
-        const diff = catsOnScreen - newCatsOnScreen;
+  if (newCatsOnScreen < catsOnScreen) {
+    const diff = catsOnScreen - newCatsOnScreen;
 
-        for (let i = 0; i < diff; i++) {
-            const imgs = document.querySelectorAll('img');
-            const img = imgs[0];
-            img.parentNode.removeChild(img);
-        }
+    for (let i = 0; i < diff; i++) {
+      const imgs = document.querySelectorAll("img");
+      const img = imgs[0];
+      img.parentNode.removeChild(img);
     }
+  }
 
-    catsOnScreen = newCatsOnScreen;
+  catsOnScreen = newCatsOnScreen;
 }
 
-
 function createSpiningCat(container) {
-    const img = document.createElement('img');
-    // 192 x 128
-    img.setAttribute('src', 'https://cdn.7tv.app/emote/636531e48ed289fb207f21e1/4x.webp');
-    img.setAttribute('alt', 'Rupert\'s spinning so sweetly, pretty, huh?');
+  const img = document.createElement("img");
+  // 192 x 128
+  img.setAttribute(
+    "src",
+    "https://cdn.7tv.app/emote/636531e48ed289fb207f21e1/4x.webp",
+  );
+  img.setAttribute("alt", "Rupert's spinning so sweetly, pretty, huh?");
 
-    container.appendChild(img);
+  container.appendChild(img);
 }
